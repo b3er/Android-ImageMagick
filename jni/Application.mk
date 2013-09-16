9d1713cd52cabafdc0e7bc82cfae9969255b5f9a
b3er <b3er@who.ms>
2013-09-17 00:56:03 +0400

    initial, limited tiff support

---
+++ jni/Application.mk
@@ -0,0 +1,8 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+APP_ABI := all
+APP_STL := gnustl_static
+APP_PLATFORM := android-8
+
+
